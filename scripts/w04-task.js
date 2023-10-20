701b9b5ac423ec1e1446ff8ee3113d2f2158ef60
mak-phi <mkmakiti@gmail.com>
2023-10-20 08:27:24 +0300

    w04-assignment

---
+++ scripts/w04-task.js
@@ -0,0 +1,63 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+const myProfile = {
+    name: "Roy Makau Mutua",
+    photo: "../images/profile.jpg",
+    favoriteFoods: ["Fried fish", "Ugali", "Kachumbari", "Lemon squeeze", "Sugar cane juice"],
+    hobbies: ["Gardening", "Watching movies", "Listening to music", "Playing chess"],
+    placesLived: [],
+};
+
+
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: "Nairobi, Kenya",
+        length: "34 years",
+    },
+    {
+        place: "Eldoret, Kenya",
+        length: "4 years",
+    },
+    {
+        place: "Thika, Kenya",
+        length: "4 years",
+    });
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.getElementById("name").textContent = myProfile.name;
+
+/* Photo with attributes */
+document.getElementById("photo").src = myProfile.photo;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach((food) => {
+    let listElement = document.createElement("li");
+    listElement.textContent = food;
+    document.getElementById("favorite-foods").appendChild(listElement);
+});
+
+/* Hobbies List */
+myProfile.hobbies.forEach((hobby) => {
+    let listElement = document.createElement("li");
+    listElement.textContent = hobby;
+    document.getElementById("hobbies").appendChild(listElement);
+});
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach((obj) => {
+    let dt = document.createElement("dt");
+    dt.textContent = obj.place;
+    document.getElementById("places-lived").appendChild(dt);
+
+    let dd = document.createElement("dd");
+    dt.textContent = obj.length;
+    document.getElementById("places-lived").appendChild(dd);
+});
+
